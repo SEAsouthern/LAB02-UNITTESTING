4e45d868e801337b522bc030c614e8845fa05766
Matthew Johnson <matthew.james.johnson@hotmail.com>
2020-03-19 19:57:35 -0700

    Deposit method added.

--- LAB02-UNITTESTING/Program.cs
+++ LAB02-UNITTESTING/Program.cs
@@ -16,11 +16,16 @@ namespace LAB02_UNITTESTING
             Console.WriteLine("Welcome to your Bank");
             Console.WriteLine("What would you like to do?");
             Console.WriteLine("Press 1 to Check Balance.");
+            Console.WriteLine("Press 3 to Deposit money.");
             string UserChoice = Console.ReadLine();
             int UserChoiceInt = Convert.ToInt32(UserChoice);
             if (UserChoiceInt == 1)
             {
                 ReadBalance();
+            }
+            else if (UserChoiceInt == 3)
+            {
+                Deposit();
             }
 
 
@@ -31,6 +36,26 @@ namespace LAB02_UNITTESTING
         public static void ReadBalance()
         {
             Console.WriteLine($"Your balance is {Program.Balance}.");
+        }
+        /// <summary>
+        /// This method gives the user the ability to deposit money to their account from the ATM terminal.
+        /// </summary>
+        public static void Deposit()
+        {
+            Console.WriteLine("How much would you like to deposit?");
+            string UserDeposit = Console.ReadLine();
+            int DepositInt = Convert.ToInt32(UserDeposit);
+            if (DepositInt < 0)
+            {
+                Console.WriteLine("That is not a valid amount.");
+                Console.Clear();
+                Deposit();
+            }
+            else
+            {
+                Balance += DepositInt;
+                Console.WriteLine($"Your new balance is {Balance}");
+            }
         }
     }
 }
