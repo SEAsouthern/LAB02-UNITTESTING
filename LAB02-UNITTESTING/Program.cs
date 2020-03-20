f891039c4e49d59981bdda2288b3354a5025779f
Matthew Johnson <matthew.james.johnson@hotmail.com>
2020-03-19 19:34:47 -0700

    Began Start method, completed ReadBalance method.

--- LAB02-UNITTESTING/Program.cs
+++ LAB02-UNITTESTING/Program.cs
@@ -3,10 +3,34 @@
 namespace LAB02_UNITTESTING
 {
     class Program
-    {
+
+    {   //Initial balance for ATM program
+        static int Balance = 3000;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Start();
+        }
+
+        public static void Start()
+        {
+            Console.WriteLine("Welcome to your Bank");
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("Press 1 to Check Balance.");
+            string UserChoice = Console.ReadLine();
+            int UserChoiceInt = Convert.ToInt32(UserChoice);
+            if (UserChoiceInt == 1)
+            {
+                ReadBalance();
+            }
+
+
+        }
+        /// <summary>
+        /// This method gives the user the ability to check their balance from the ATM menu. 
+        /// </summary>
+        public static void ReadBalance()
+        {
+            Console.WriteLine($"Your balance is {Program.Balance}.");
         }
     }
 }
